247ee07669c1cb6249316ec880ef4197ba44594f
andrewhsiao11 <90870850+andrewhsiao11@users.noreply.github.com>
2022-03-30 16:16:38 -0400

    backend established

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("server")
+const express = require('express')
+const colors = require("colors");
+const dotenv = require("dotenv").config()
+const {errorHandler} = require('./middleware/errorMiddleWare')
+const connectDb = require('./config/db')
+const PORT = process.env.PORT || 5050
+
+//Connect to database
+connectDb();
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Welcome to the Support Desk API'})
+})
+
+// Routes - connects /api/users to that userRoutes file
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler);
+
+
+app.listen(PORT, () => console.log(`server started  ${PORT}`))
